fa25d1912d423bff668ce6281dcd945e510e7e7e
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-01-12 16:43:37 +0100

    fix on systemd restart

--- recipes/_default.rb
+++ recipes/_default.rb
@@ -68,7 +68,7 @@ if node.dela.systemd == "true"
   end
 
   # This is needed so that the service can be started by systemd
-  hadoop_spark_start "reload_#{service_name}" do
+  kagent_config "reload_#{service_name}" do
     action :systemd_reload
   end  
 
