50242fa33e0d9d7471ead5a3e02420921691470a
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-05-04 20:16:59 +0200

    fix

--- recipes/install.rb
+++ recipes/install.rb
@@ -77,3 +77,66 @@ link node.dela.base_dir do
   to node.dela.home
 end
 
+if node.dela.id.nil?
+    node.override.dela.id = Time.now.getutc.to_i
+end
+
+if node.dela.seed.nil?
+    node.override.dela.seed = Random.rand(100000)
+end
+
+# hack until groups attrs work in karamel
+stun1_ip = "192.168.56.102"
+stun2_ip = "192.168.56.103"
+stun1_id = 2
+stun2_id = 3
+
+if my_private_ip().eql?("192.168.56.104")
+  node.override.dela.port = 41100
+  node.override.dela.stun_port1 = 41101
+  node.override.dela.stun_port2 = 41102
+  node.override.dela.hops.storage.type = "DISK"
+  node.override.dela.hops.library.type = "DISK"
+end
+if my_private_ip().eql?("192.168.56.105")
+  node.override.dela.port = 41200
+  node.override.dela.stun_port1 = 41201
+  node.override.dela.stun_port2 = 41202
+  node.override.dela.hops.storage.type = "DISK"
+  node.override.dela.hops.library.type = "DISK"
+end
+########
+
+#raise if node.dela.stun_servers_ip.size < 2 
+#stun1_ip = node.dela.stun_servers_ip[0]
+#stun2_ip = node.dela.stun_servers_ip[1]
+#stun1_id = node.dela.stun_servers_id[0]
+#stun2_id = node.dela.stun_servers_id[1]
+
+template "#{node.dela.home}/conf/application.conf" do
+  source "application.conf.erb" 
+  owner node.dela.user
+  group node.dela.group
+  mode 0750
+  variables({ 
+     :stun1_ip => stun1_ip,
+     :stun2_ip => stun2_ip,
+     :stun1_id => stun1_id,
+     :stun2_id => stun2_id
+  })
+end
+
+template "#{node.dela.home}/conf/config.yml" do
+  source "config.yml.erb" 
+  owner node.dela.user
+  group node.dela.group
+  mode 0750
+end
+
+if node.kagent.enabled == "true" 
+   kagent_config node.dela.service do
+     service node.dela.service
+     log_file "#{node.dela.logs}"
+     config_file "#{node.dela.home}/conf/application.conf"
+   end
+end
