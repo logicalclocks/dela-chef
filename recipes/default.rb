f2149d06c6ae7e92dd8d0c648387470c2d7436c3
Jim Dowling <jdowling@sics.se>
2018-01-30 13:56:46 +0100

    change kagent group for the jhs (#6)

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@ include_recipe "dela::base-default"
 service_name=node['dela']['service']
 if node['kagent']['enabled'] == "true" 
    kagent_config service_name do
-     service service_name
+     service "Hops"
      log_file "#{node['dela']['logs']}"
      config_file "#{node['dela']['home']}/conf/application.conf"
    end
