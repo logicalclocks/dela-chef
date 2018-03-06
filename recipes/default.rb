40d848d77e1e35abc2f30859dd1db0b2ed7fcc62
Jim Dowling <jdowling@sics.se>
2018-03-06 10:18:57 +0100

    cleanup

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,64 @@
-include_recipe "dela::base-default"
 
 service_name=node['dela']['service']
+
+case node['platform']
+when "ubuntu"
+ if node['platform_version'].to_f <= 14.04
+   node.override['dela']['systemd']=node_systemd = "false"
+ end
+end
+
+node_systemd=node['dela']['systemd']
+
+if node_systemd == "true"
+  service service_name do
+    provider Chef::Provider::Service::Systemd
+    supports :restart => true, :stop => true, :start => true, :status => true
+    action :nothing
+  end
+
+  case node['platform_family']
+  when "rhel"
+    systemd_script = "/usr/lib/systemd/system/#{service_name}.service" 
+  else
+    systemd_script = "/lib/systemd/system/#{service_name}.service"
+  end
+
+  template systemd_script do
+    source "#{service_name}.service.erb"
+    owner "root"
+    group "root"
+    mode 0754
+    notifies :enable, resources(:service => service_name)
+    notifies :start, resources(:service => service_name), :immediately
+  end
+
+  # This is needed so that the service can be started by systemd
+  kagent_config "#{service_name}" do
+    action :systemd_reload
+  end  
+
+else #sysv
+  service service_name do
+    provider Chef::Provider::Service::Init::Debian
+    supports :restart => true, :stop => true, :start => true, :status => true
+    action :nothing
+  end
+
+  template "/etc/init.d/#{service_name}" do
+    source "#{service_name}.erb"
+    owner "root"
+    group "root"
+    mode 0754
+    notifies :enable, resources(:service => service_name)
+    notifies :restart, resources(:service => service_name), :immediately
+  end
+end
+
 if node['kagent']['enabled'] == "true" 
    kagent_config service_name do
      service "Hops"
      log_file "#{node['dela']['logs']}"
-     config_file "#{node['dela']['home']}/conf/application.conf"
+     config_file "#{node['dela']['base_dir']}/conf/application.conf"
    end
 end
