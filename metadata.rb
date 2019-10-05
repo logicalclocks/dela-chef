a1d45d255f56d723fa4e7cec3cbd98679fcc0387
Theofilos Kakantousis <t.kakantousis@gmail.com>
2019-10-05 16:26:55 +0200

    Release version: 1.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Alex Ormenisan"
 maintainer_email "aaor@kth.se"
 license          "Apache v2.0"
 description      'Installs/Configures/Runs dela'
-version          "1.0.0"
+version          "1.1.0"
 
 recipe            "dela::install",      "Install dela binaries"
 recipe            "dela::default",      "Basic dela recipe"
