4c918a370f703a726996c005dde62adb09008e91
Fabio Buso <buso.fabio@gmail.com>
2020-01-26 19:21:40 +0100

    Release version: 1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Alex Ormenisan"
 maintainer_email "aaor@kth.se"
 license          "Apache v2.0"
 description      'Installs/Configures/Runs dela'
-version          "1.2.0"
+version          "1.3.0"
 
 recipe            "dela::install",      "Install dela binaries"
 recipe            "dela::default",      "Basic dela recipe"
