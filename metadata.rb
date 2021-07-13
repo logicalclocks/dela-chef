a59fe220572354c9dd00131068598a1afdbbabfd
tkakantousis <t.kakantousis@gmail.com>
2021-07-13 16:01:13 +0200

    Release version: 2.3

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Alex Ormenisan"
 maintainer_email "aaor@kth.se"
 license          "Apache v2.0"
 description      'Installs/Configures/Runs dela'
-version          "2.3.0"
+version          "3.0.0"
 
 recipe            "dela::install",      "Install dela binaries"
 recipe            "dela::default",      "Basic dela recipe"
